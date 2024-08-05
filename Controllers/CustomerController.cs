6d65423d38f0ce99cd817ec53b4300c9e94fb4f7
Victoria Misyakova <vmisyakova@gmail.com>
2024-08-05 12:16:01 +0300

    Добавьте файлы проекта.

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,33 @@
+﻿using coreWebApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace coreWebApplication.Controllers
+{
+	public class CustomerController : Controller
+	{
+		public static List<Customer> customers = new List<Customer>()
+		{
+			new Customer() { Id = 101, Name = "King", Amount = 12000},
+			new Customer() { Id = 102, Name = "King", Amount = 12000}
+		};
+		public IActionResult Index()
+		{
+			ViewBag.Message = "Customer Management Sysytem";
+			ViewBag.CustomerCount = customers.Count();
+			ViewBag.CustomerList = customers;
+			return View();
+		}
+
+		public IActionResult Details()
+		{
+			return View();
+		}
+
+		//[Route("~/")]
+		[Route("/sample/message")]
+		public IActionResult Message()
+		{
+			return View();
+		}
+	}
+}
---
+++ Controllers/DemoController.cs
@@ -0,0 +1,71 @@
+﻿using coreWebApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace coreWebApplication.Controllers
+{
+    public class DemoController : Controller
+    {
+        public static List<Customer> customers = new List<Customer>()
+        {
+            new Customer() { Id = 101, Name = "King", Amount = 12000},
+            new Customer() { Id = 102, Name = "King", Amount = 12000}
+        };
+        public IActionResult Index()
+        {
+            ViewBag.Message = "Customer Management Sysytem";
+            ViewBag.CustomerCount = customers.Count();
+            ViewBag.CustomerList = customers;
+            return View();
+        }
+
+        public IActionResult Details()
+        {
+            ViewData["Message"] = "Customer Management Sysytem";
+            ViewData["CustomerCount"] = customers.Count();
+            ViewData["CustomerList"] = customers;
+            return View();
+        }
+
+        public IActionResult Method1()
+        {
+            TempData["Message"] = "Customer Management Sysytem";
+            TempData["CustomerCount"] = customers.Count();
+            TempData["CustomerList"] = customers;
+            return View();
+        }
+
+        public IActionResult Method2()
+        {
+            if (TempData["Message"] == null)
+                return RedirectToAction("Index");
+            TempData["Message"] = TempData["Message"].ToString();
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            HttpContext.Session.SetString("username", "Victoria");
+            return RedirectToAction("Success");
+        }
+
+        public IActionResult Success()
+        {
+            ViewBag.Username = HttpContext.Session.GetString("username");
+            return View();
+        }
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Remove("username");
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult QueryTest()
+        {
+            string name = "King Kochhar";
+            if (!String.IsNullOrEmpty(HttpContext.Request.Query["name"]))
+                name = HttpContext.Request.Query["name"];
+            ViewBag.Name = name;
+            return View();
+        }
+    }
+}
